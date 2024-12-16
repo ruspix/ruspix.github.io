31437fe64a45b8c8d4c66641144c60477f812c59
o/ <68079109+FurnyGo@users.noreply.github.com>
2024-12-16 12:34:49 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const tabs = document.querySelectorAll('.tab');
     const home = document.getElementById('home');
     const instructions = document.getElementById('instructions');
+    const news = document.getElementById('news');
 
     tabs.forEach(tab => {
         tab.addEventListener('click', function (event) {
@@ -10,12 +11,18 @@ document.addEventListener('DOMContentLoaded', function () {
             tabs.forEach(t => t.classList.remove('active'));
             tab.classList.add('active');
 
-            if (tab.innerText === "Инструкция по установке") {
+            if (tab.innerText === "Инструкция по установке новостей") {
                 home.style.display = "none";
                 instructions.style.display = "block";
+                news.style.display = "none";
+            } else if (tab.innerText === "Новости") {
+                home.style.display = "none";
+                instructions.style.display = "none";
+                news.style.display = "block";
             } else {
                 home.style.display = "block";
                 instructions.style.display = "none";
+                news.style.display = "none";
             }
         });
     });
