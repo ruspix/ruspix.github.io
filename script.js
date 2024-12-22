a0a2a267ba5ff2bd45a6c6f97e4711c8b739ee73
o/ <68079109+FurnyGo@users.noreply.github.com>
2024-12-22 14:54:13 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const tabs = document.querySelectorAll('.tab');
     const home = document.getElementById('home');
-    const instructions = document.getElementById('instructions');
     const guides = document.getElementById('guides');
 
     tabs.forEach(tab => {
@@ -11,19 +10,26 @@ document.addEventListener('DOMContentLoaded', function () {
             tabs.forEach(t => t.classList.remove('active'));
             tab.classList.add('active');
 
-            if (tab.innerText === "Инструкция по скрипту") {
+            if (tab.innerText === "Гайды") {
                 home.style.display = "none";
-                instructions.style.display = "block";
-                guides.style.display = "none";
-            } else if (tab.innerText === "Мини-гайды") {
-                home.style.display = "none";
-                instructions.style.display = "none";
                 guides.style.display = "block";
             } else {
                 home.style.display = "block";
-                instructions.style.display = "none";
                 guides.style.display = "none";
             }
         });
     });
 });
+
+function openGuide(guide) {
+    document.getElementById('overlay').style.display = 'flex';
+    fetch(`https://raw.githubusercontent.com/ruspix/ruspix.github.io/refs/heads/main/guides/${guide}.html`)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('guide-text').innerHTML = data;
+        });
+}
+
+function closeGuide() {
+    document.getElementById('overlay').style.display = 'none';
+}
